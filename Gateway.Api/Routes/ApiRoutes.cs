8314128071bcebae64f70fa65db99f52e4de7740
NicanorXd <nicanor200026@gmail.com>
2023-02-04 17:44:53 -0500

    Falta implementar gateway

---
+++ Gateway.Api/Routes/ApiRoutes.cs
@@ -0,0 +1,23 @@
+﻿namespace Material.Api.Routes
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        public static class RouteMaterial
+        {
+            // Read
+            public const string GetAll = Base + "/material/all";
+            public const string GetById = Base + "/material/{id}";
+
+            // Write
+            public const string Create = Base + "/material/create";
+            public const string Update = Base + "/material/update";
+            public const string Delete = Base + "/material/delete";
+
+        }
+
+    }
+}
---
+++ Gateway.Aplication/DependencyInjection.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using MongoDB.Driver;
+using Release.MongoDB.Repository;
+using Material.Aplicacion.Material;
+using Material.Infraestructura;
+using dominio = Material.Dominio.Entidades;
+
+namespace Material.Aplicacion
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddAplicacion(this IServiceCollection services, IConfiguration configuration) 
+        {
+            #region Mongo
+
+            string cs = configuration.GetSection("DBSettings:ConnectionString").Value;
+            var dbUrl = new MongoUrl(cs);
+
+            services.AddScoped<IDbContext>(x => new DbContext(dbUrl));
+
+            //Entidades            
+            services.TryAddScoped<ICollectionContext<dominio.Material>>(x => new CollectionContext<dominio.Material>(x.GetService<IDbContext>()));
+            //services.TryAddScoped<ICollectionContext<dominio.Categoria>>(x => new CollectionContext<dominio.Categoria>(x.GetService<IDbContext>()));
+
+            //Como Repo
+            services.TryAddScoped<IBaseRepository<dominio.Material>>(x => new BaseRepository<dominio.Material>(x.GetService<IDbContext>()));
+            //services.TryAddScoped<IBaseRepository<dominio.Categoria>>(x => new BaseRepository<dominio.Categoria>(x.GetService<IDbContext>()));
+
+            #endregion
+
+            #region Servicios
+
+            services.AddScoped<IMaterialService, MaterialService>();
+            //services.AddScoped<ICategoriaService, CategoriaService>();
+
+            #endregion
+
+            return services;
+        }
+
+    }
+}
